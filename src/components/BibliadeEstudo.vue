c68b61d58fe4bcb098b80eb4ab159449d91d1716
Alexandre Braga <albraga@email.com>
2017-03-03 08:15:07 -0400

    add LibreBaskerville ttf

--- src/components/BibliadeEstudo.vue
+++ src/components/BibliadeEstudo.vue
@@ -5,18 +5,10 @@
 				<div class="col-xs-3 col-sm-3 col-md-3">
 				</div>
 				<div class="col-xs-9 col-sm-9 col-md-9">
-					<div class="panel panel-default">
-						<div class="panel-heading">{{ this.$store.state.verse.verseId }}</div>
-						<div class="panel-body">
-							{{ this.$store.state.verse.verseText }}
-						</div>
-						<div class="panel-footer">
-							<details>
-								<summary>Read more</summary>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra nec nulla vitae mollis.</p>
-							</details>
-						</div>
-					</div>
+					<dl class="dl-horizontal">
+  					<dt>{{ this.$store.state.verse.verseId }}</dt>
+  					<dd>{{ this.$store.state.verse.verseText }}</dd>
+					</dl>
 				</div>
 			</div>
 		</div>
@@ -47,5 +39,11 @@
 
 <style>
 	@import url('../../static/bootstrap/css/bootstrap.min.css');
-
+	@font-face {
+  	font-family: 'LibreBaskerville';
+  	src: url('../../static/LibreBaskerville-Regular.ttf');
+	}
+	.dl-horizontal { 
+		font-family: 'LibreBaskerville'; 
+	}
 </style>
